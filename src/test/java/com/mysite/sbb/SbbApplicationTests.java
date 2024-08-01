01c3973ecea8762c7f18ff9094ae71ff8847d8c1
unknown <jangka512@gmail.com>
2024-08-01 13:32:05 +0900

    work

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -4,6 +4,7 @@ import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.answer.AnswerRepository;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +19,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SappApplicationTests {
+    @Autowired
+    private QuestionService questionService;
 
     @Autowired
     private QuestionRepository questionRepository;
@@ -130,5 +133,14 @@ class SappApplicationTests {
         assertEquals(1, answerList.size());
 
         assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+    }
+
+    @Test
+    void testJpa12() {
+        for (int i = 1; i <= 300; i++) {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content = "내용무";
+            questionService.create(subject, content);
+        }
     }
 }
